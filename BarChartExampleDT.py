dfdfc435fb75fe57e32c78f7ad3c4cb6fd5c896b
thomarie <dthomarie@gmail.com>
2017-11-08 22:08:03 -0500

    instructions added
    instructions added as comments following the Part 1 code

--- BarChartExampleDT.py
+++ BarChartExampleDT.py
@@ -7,3 +7,7 @@ import matplotlib.pyplot as plt
 objects = ('Uncharted 4', 'Doom', 'Minecraft', 'LoL', 'AC Syndicate', 'FIFA17')
 y_pos = np.arange(len(objects))
 performance = [120,80,60,40,20,18]
+
+#instructions:
+#add the rest of the code below these comments
+#delete these instructions (lines 11,12,13) when you are done
